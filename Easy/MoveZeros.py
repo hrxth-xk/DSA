50e18e64909184ffe2104175af9119484edf0aa7
Hrithik Reddy Chenreddy <122119750+hrxth-xk@users.noreply.github.com>
2026-01-18 13:21:38 +0530

    Add moveZeroes function to shift zeros in list
    Implement function to move all zeros in-place in the list.

---
+++ Easy/MoveZeros.py
@@ -0,0 +1,16 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j=0
+        for i in range(1,len(nums)):
+            if nums[i] !=0 and nums[j]==0:
+                nums[j],nums[i]=nums[i],nums[j] #might have to do opp here
+                j+=1
+            elif nums[i]==0 and nums[j]==0:
+                continue
+            else:
+                j+=1
+            
+        
